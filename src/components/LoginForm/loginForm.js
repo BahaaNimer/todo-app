266cd37f326e889809cfd5af409bb680d36b71dc
BahaaNimer <bahaanimer97@gmail.com>
2022-08-30 01:07:27 +0300

    finish task 33

---
+++ src/components/LoginForm/loginForm.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from "react";
+import "./loginForm.css"
+import { LoginContext } from "../context/Login";
+import { When } from "react-if";
+
+export default function LoginForm(props) {
+  const loginContext = useContext(LoginContext);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    loginContext.login(username, password);
+  };
+  const usernameHandler = (e) => {
+    e.preventDefault();
+    setUsername(e.target.value);
+  };
+  const passwordHandler = (e) => {
+    e.preventDefault();
+    setPassword(e.target.value);
+  };
+  return (
+    <>
+      <When condition={!loginContext.loggedIn}>
+        <div className="card-form">
+          <form className="form-login">
+            <div className="input">
+              <label>Username </label>
+              <input type="text" placeholder="Enter your username" name="uname" required onChange={usernameHandler} />
+            </div>
+
+            <div className="input">
+              <label>Password </label>
+              <input type="password" placeholder="Enter your password" name="pass" required onChange={passwordHandler} />
+            </div>
+
+            <div className="button">
+              <button type="submit" onClick={handleSubmit}>Log In</button>
+            </div>
+          </form>
+        </div>
+      </When>
+
+    </>
+  );
+}
